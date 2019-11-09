f6f4a2719ac3ab55762d0a831f5510ef4a9d23d8
StrangeAlmond <echurch05@yahoo.com>
2019-11-08 17:43:15 -0800

    Adds a function to the functions file

--- utils/functions.js
+++ utils/functions.js
@@ -2,5 +2,9 @@ module.exports = {
   convertFromCamelCase: function (string) {
     const result = string.replace(/([A-Z0-9])/g, " $1");
     return result.charAt(0).toUpperCase() + result.slice(1);
+  },
+
+  capitalizeFirstLetter: function (string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
   }
 };
